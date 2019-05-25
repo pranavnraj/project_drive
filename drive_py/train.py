94744d61a9ed58b60dfad29e06fb9a8b11c8d60a
Falafel72 <kylehu2000@gmail.com>
2019-05-25 23:15:16 +0000

    more bug fixes and refactoring

--- drive_py/train.py
+++ drive_py/train.py
@@ -1,13 +1,11 @@
-import tensorflow as tf
+import tensorflow as tf 
 from tensorflow import keras
+#from keras.backend.tensorflow_backend import set_session
 
 import logging
 import cv2
 import numpy as np
 import os
-
-# Set up logger
-logging.basicConfig(filename='train.log', level=logging.DEBUG);
 
 # Load images and labels
 def load_data(folder_name):
@@ -23,38 +21,49 @@ def load_data(folder_name):
 		labels.append(label)
 	images = np.asarray(images)
 	images = images / 255.0
-	labels = np.asarray(train_labels)
+	labels = np.asarray(labels)
 	return images,labels
 
 
+def train():
+	# Create the neural network
+	model = keras.Sequential([
+		keras.layers.Flatten(input_shape=(28,28)),
+		keras.layers.Dense(128, activation=tf.nn.relu),
+		keras.layers.Dense(9, activation=tf.nn.softmax)
+	])
 
-# Create the neural network
-model = keras.Sequential([
-	keras.layers.Flatten(input_shape=(28,28)),
-	keras.layers.Dense(128, activation=tf.nn.relu),
-	keras.layers.Dense(9, activation=tf.nn.softmax)
-])
+	# TODO: Determine what settings work best for our data set
+	model.compile(optimizer='adam',
+		loss='sparse_categorical_crossentropy',
+		metrics=['accuracy'])
 
-# TODO: Determine what settings work best for our data set
-model.compile(optimizer='adam',
-	loss='sparse_categorical_crossentropy',
-	metrics=['accuracy'])
+	train_images, train_labels = load_data("processed_data")
 
-train_images, train_labels = load_train_data("processed_data")
+	test_images, test_labels = load_data("test_data")
 
-test_images, test_labels = load_test_data("test_data")
+	model.fit(train_images, train_labels, epochs=5)
 
-model.fit(train_images, train_labels, epochs=5)
+	test_loss, test_acc = model.evaluate(test_images, test_labels);
 
-test_loss, test_acc = model.evaluate(test_images, test_labels);
+	if( test_acc >= 0.85):
+		logging.info("Reached accuracy threshold")
+	else:
+		logging.warning("Below accuracy threshold!")
 
-if( test_acc >= 0.85):
-	logging.info("Reached accuracy threshold")
-else
-	logging.warning("Below accuracy threshold!")
+	model.save('pathtracker.h5')
 
-model.save('pathtracker.h5')
+# limit memory allocated to jetson
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
 
-def predict():
-	predictions = model.predict(#test_images);
+session = tf.Session(config=config)
+
+# Set up logger
+logging.basicConfig(filename='train.log', level=logging.DEBUG);
+
+train()
+
+
+
 
