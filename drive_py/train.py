7708cd40f58d91e972d3c887269e72d767286e52
Falafel72 <kylehu2000@gmail.com>
2019-05-25 01:44:56 -0700

    fixed predict and training code

--- drive_py/train.py
+++ drive_py/train.py
@@ -10,23 +10,22 @@ import os
 logging.basicConfig(filename='train.log', level=logging.DEBUG);
 
 # Load images and labels
-def load_train_data():
-	train_images = []
-	train_labels = []
+def load_data(folder_name):
+	images = []
+	labels = []
 	current_path = os.getcwd()
-	img_folder_path = os.path.join(current_path, "processed_data")
+	img_folder_path = os.path.join(current_path, folder_name)
 	for img_name in os.listdir(img_folder_path):
 		label = int(img_name[:-4].split('_')[1])
 		img_path = os.path.join(img_folder_path, img_name)
 		img = cv2.imread(img_path, cv2.CV_8UC1)
-		train_images.append(img)
-		train_labels.append(label)
-	train_images = np.asarray(train_images)
-	train_images = train_images / 255.0
-	train_labels = np.asarray(train_labels)
-	return train_images,train_labels
+		images.append(img)
+		labels.append(label)
+	images = np.asarray(images)
+	images = images / 255.0
+	labels = np.asarray(train_labels)
+	return images,labels
 
-	
 
 
 # Create the neural network
@@ -41,15 +40,17 @@ model.compile(optimizer='adam',
 	loss='sparse_categorical_crossentropy',
 	metrics=['accuracy'])
 
-train_images, train_labels = load_train_data()
+train_images, train_labels = load_train_data("processed_data")
+
+test_images, test_labels = load_test_data("test_data")
 
 model.fit(train_images, train_labels, epochs=5)
 
-test_loss, test_acc = model.evaluate(#TODO Fill this with test data# );
+test_loss, test_acc = model.evaluate(test_images, test_labels);
 
 if( test_acc >= 0.85):
 	logging.info("Reached accuracy threshold")
-else 
+else
 	logging.warning("Below accuracy threshold!")
 
 model.save('pathtracker.h5')
